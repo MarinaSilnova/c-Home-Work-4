0431cdfec349281e65abd0a0af73836b6c101bfc
Marina Silnova <marusia421@mail.ru>
2022-09-14 16:42:37 +0300

    Решение дополнительной задачи

---
+++ Доп.задача/Program.cs
@@ -0,0 +1,18 @@
+﻿// 
+
+int max = 0;
+int[] n = new int[8];
+for (int i = 0; i < n.Length; i++)
+{
+    n[i] = new Random().Next(1, 1001);
+    for (int j = 2; j <= i; j++)
+{
+    if ((n[j] + n[j - 1] + n[j - 2]) > max)
+        max = n[j] + n[j - 1] + n[j - 2];
+}   
+}
+Console.WriteLine ( "[" + string.Join(", ", n) + "]");
+Console.WriteLine(max);
+
+
+
