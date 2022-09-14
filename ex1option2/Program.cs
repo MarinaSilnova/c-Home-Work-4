daa1d3cdec5fef916d54134381c036a90a85b477
Marina Silnova <marusia421@mail.ru>
2022-09-14 10:33:51 +0300

    Добавили второй вариант решения первой задачи

---
+++ ex1option2/Program.cs
@@ -0,0 +1,7 @@
+﻿// 2-ой вариант решения задачи:  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Write(" Введите значение числа A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите значение числа B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+double Sq = 1;
+Console.WriteLine(Sq = Math.Pow(A, B));
