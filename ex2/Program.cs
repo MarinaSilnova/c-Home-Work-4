52433c02d290b7de66ca1b91ace638455d2d49ee
Marina Silnova <marusia421@mail.ru>
2022-09-14 10:52:09 +0300

    add ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write(" Введите  число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (n > 0)
+{
+    sum = sum + (n % 10);
+    n /= 10;
+}
+Console.WriteLine(sum);
