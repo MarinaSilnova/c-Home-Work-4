e0df3ce883e0c76a60140284ff8162d56153e843
Marina Silnova <marusia421@mail.ru>
2022-09-14 10:03:26 +0300

    add ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write(" Введите значение числа A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите значение числа B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int Sq = 1;
+for(int i = 1; i <= B; i++)
+    Sq = Sq * A;
+Console.WriteLine(Sq);
